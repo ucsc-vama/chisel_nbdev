cbffbe28af24176a02730ab9ba9299b224aa5cc7
jasonvranek <jvranek@ucsc.edu>
2021-01-29 17:25:06 -0800

    dummy script to populate build sbt project structure

---
+++ build_project.sh
@@ -0,0 +1,5 @@
+mkdir -p $PROJ_NAME/src/{main,test}/{resource,scala,java}
+mkdir $PROJ_NAME/project 
+touch $PROJ_NAME/project/build.properties
+touch $PROJ_NAME/project/plugins.sbt
+touch $PROJ_NAME/build.sbt
