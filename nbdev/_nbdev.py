bf74eed6c44f856bfe0351d34a5a51c7460e3ca4
jasonvranek <jvranek@ucsc.edu>
2021-02-04 17:51:57 -0800

    fixed scala regex that was missing class definitions without parameters

--- nbdev/_nbdev.py
+++ nbdev/_nbdev.py
@@ -3,6 +3,10 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"Add": "test.ipynb",
+         "ComposedModule": "import_chisel_mod.ipynb",
+         "DoubleComposedMod": "import_composed_mod.ipynb",
+         "ALUIO": "test.ipynb",
+         "ALUSkeleton": "test.ipynb",
          "add": "test.ipynb",
          "Sub": "test.ipynb",
          "sub": "test.ipynb",
@@ -12,12 +16,15 @@ index = {"Add": "test.ipynb",
          "includeAdd": "test.ipynb",
          "includeSub": "test.ipynb",
          "includeMulDiv": "test.ipynb",
-         "Operator": "test.ipynb"}
+         "Operator": "test.ipynb",
+         "FuncFromTest": "test.ipynb",
+         "FuncToNewScript": "test.ipynb"}
 
 modules = ["ModA.sc",
            "ModB.sc",
            "ModC.sc",
-           "test.sc"]
+           "test.sc",
+           "NewScript.sc"]
 
 doc_url = "tbd/"
 
